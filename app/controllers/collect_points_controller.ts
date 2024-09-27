e914fe2a6c2491c8f784e466cb074abf0cecf08f
Emanuel Moura <emanuel.moura@ccc.ufcg.edu.br>
2024-09-27 15:00:07 -0300

    feat(edition): create controllers

--- app/controllers/collect_points_controller.ts
+++ app/controllers/collect_points_controller.ts
@@ -1,4 +1,36 @@
-// import type { HttpContext } from '@adonisjs/core/http'
+import CollectPoint from '#models/collect_point'
+import type { HttpContext } from '@adonisjs/core/http'
 
 export default class CollectPointsController {
+  /**
+   * Display a list of resource
+   */
+  async index({ }: HttpContext) {
+    const collectPoints = CollectPoint.query().preload('collectTypes')
+    return await collectPoints
+  }
+
+  /**
+   * Handle form submission for the create action
+   */
+  async store({ request }: HttpContext) {
+    const body = request.body()
+    return await CollectPoint.create(body)
+  }
+
+  /**
+   * Show individual record
+   */
+  async show({ params }: HttpContext) {
+    return await CollectPoint.findOrFail(params.id)
+  }
+  
+
+  async destroy({ params }: HttpContext) {
+    const collectPoint = await CollectPoint.findOrFail(params.id)
+    await collectPoint.delete()
+    return {
+      message: 'Record deleted'
+    }
+  }
 }
---
+++ app/models/collect_point.ts
@@ -0,0 +1,48 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column, hasMany } from '@adonisjs/lucid/orm'
+import CollectType from './collect_type.js'
+import type { HasMany } from '@adonisjs/lucid/types/relations'
+
+export default class CollectPoint extends BaseModel {
+  @column({ isPrimary: true })
+  declare id: number
+
+  @column()
+  declare name: string
+
+  @column()
+  declare embedCode: string
+
+  @column()
+  declare openAt: DateTime
+
+  @column()
+  declare closeAt: DateTime
+
+  @column()
+  declare phone: string
+
+  @column()
+  declare street: string
+
+  @column()
+  declare city: string
+
+  @column()
+  declare status: string
+
+  @column()
+  declare maxCapacity: number
+
+  @column()
+  declare aditionalInfo: string
+
+  @hasMany(() => CollectType)
+  declare collectTypes: HasMany<typeof CollectType>
+
+  @column.dateTime({ autoCreate: true })
+  declare createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  declare updatedAt: DateTime
+}
